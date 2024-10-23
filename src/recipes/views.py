9b4d089a20f3843dc7baab16f3c88852944407cd
Beaugmc86 <153542400+Beaugmc86@users.noreply.github.com>
2024-10-23 13:02:09 -0700

    Implement views
    Add Welcome page, ListView, and DetailView along with images of recipes.

--- src/recipes/views.py
+++ src/recipes/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Recipe
 
 # Create your views here.
+
+# View for welcome page
 def home(request):
-  return render(request, 'recipes/recipes_home.html')
+    return render(request, 'recipes/recipes_home.html')
+
+# View recipe list
+class RecipeListView(ListView):
+    model = Recipe
+    template_name = 'recipes/main.html'  # specify main recipe list template
+
+# View recipe details
+class RecipeDetailView(DetailView):
+    model = Recipe
+    template_name = 'recipes/detail.html'  # specify reciple detail template
