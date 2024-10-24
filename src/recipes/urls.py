af476a9aee76779e0364f835546b556d5c9eb24b
Beaugmc86 <153542400+Beaugmc86@users.noreply.github.com>
2024-10-24 14:42:43 -0700

    update urls.py

--- src/recipes/urls.py
+++ src/recipes/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import RecipeListView, RecipeDetailView
+from .views import home, RecipeListView, RecipeDetailView
 
 app_name = 'recipes'
 
 urlpatterns = [
-    path('list', RecipeListView.as_view(), name="list"),
-    path("list/<pk>", RecipeDetailView.as_view(), name="detail"),
+    path("", home), # Map the home view to the root URL of the app.
+    path('list/', RecipeListView.as_view(), name="list"), # Map the RecipeListView to '/list/', named 'list'
+    path("list/<pk>/", RecipeDetailView.as_view(), name="detail"), # Map RecipeDetailView to '/list/<int:pk>/', where 'pk' is a placeholder for the recipe ID, named 'detail'.
 ]
